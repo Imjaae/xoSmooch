8f0b1012013676e9ca89c7dcee39720a636ce604
Imjaae <dlawodud96@naver.com>
2023-06-15 17:02:32 +0900

    Fix Css style

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,17 +7,10 @@ export default function Header() {
   return (
     <Wrap>
       <Title>
-        <div
-          style={{
-            marginRight: "1000px",
-            fontSize: "120px",
-          }}
-        >
-          Xo Smooch
-        </div>
-        <div>search</div>
-        <div>login</div>
-        <div>cart</div>
+        <Smooch>Xo Smooch</Smooch>
+        <Login>search</Login>
+        <Login>login</Login>
+        <Login>cart</Login>
       </Title>
       <SubTitle>
         <Shop
@@ -45,47 +38,79 @@ export default function Header() {
   );
 }
 
+const Smooch = styled.div`
+  margin-right: 45vw;
+  font-size: 7rem;
+  margin-left: 5vw;
+`;
+
 const Wrap = styled.div`
   width: 95vw;
   position: fixed;
   z-index: 1000;
 `;
 
+const Login = styled.div`
+  color: #334fb4;
+  font-size: 1rem;
+  :hover {
+    font-size: 1.2rem;
+    font-weight: 500;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+`;
+
 const Title = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 20px;
+  padding: 2vw;
   align-items: flex-end;
+  color: #334fb4;
 `;
 
 const SubTitle = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 200px;
-  padding: 20px;
-  margin-left: 80px;
+  gap: 9vw;
+  padding: 1vw;
+  margin-left: 6vw;
+  color: #334fb4;
 `;
 
 const Shop = styled.div`
-  font-size: 40px;
+  font-size: 1.8rem;
   font-weight: 400;
+  :hover {
+    font-size: 2rem;
+    font-weight: 600;
+    transition: 0.5s;
+    cursor: pointer;
+  }
 `;
 
 const Category = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 30px;
-  padding: 10px;
-  margin-left: 90px;
+  gap: 1.4vw;
+  padding: 0.5vw;
+  margin-left: 6.5vw;
   :hover {
     color: tomato;
     transition: 0.4s;
+    cursor: pointer;
   }
 `;
 
 const Part = styled.div`
-  font-size: 20px;
-  color: gray;
+  font-size: 1.2rem;
   font-weight: 200;
+  color: #334fb4;
+  :hover {
+    font-size: 1.4rem;
+    font-weight: 300;
+    transition: 0.6s;
+    cursor: pointer;
+  }
 `;
